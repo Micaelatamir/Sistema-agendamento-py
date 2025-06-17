6bfb34e9bcddd8bf8e3c360f70623cde1eb22d06
Micaela Tamires <micaela.tamir@gmail.com>
2025-06-17 19:25:51 -0300

    update main

--- main/main.py
+++ main/main.py
@@ -3,32 +3,29 @@ import agendamento
 
 print('Menu')
 print('Escolhe a função que deseja: ')
-print('(1)Criar usuario')
+print('(1) Criar usuário')
 print('(2) Agendar compromisso')
 print('(3) Sair do sistema')
 opcao = input('Escolha uma opção:')
 
 if opcao == "1":
-  usuario = print(' Menu usuario: ')
-  print('(1 )criar usuario')
-  print('(2)listar')
-  print('(3)buscar')
-  print('(4)delentar')
-  escolha_do_usuario = input(' Escolha uma opção')
+  print(' Menu usuário: ')
+  print('(1) Criar usuário')
+  print('(2) Listar usuário')
+  print('(3) Buscar usuário')
+  print('(4) deletar usuário')
+  usuario = input(' Escolha uma opção')
 
-if escolha_do_usuario == "1" :
-  criar_usuario = input('Criar usuario: ')
-elif escolha_do_usuario == "2" :
-  listar = input('lista:  ')
-elif escolha_do_usuario == "3" :
-  escolha = input( 'escolha: ')
-elif escolha_do_usuario == "4" :
-  deletar = input('deletar usuario')  
-
-
-
+if usuario == "1" :
+  criar_usuario = input()
+elif usuario == "2" :
+  listar_usuario = input()
+elif usuario == "3" :
+  escolha = input( )
+elif usuario == "4" :
+  deletar_usuario = input()  
 elif opcao == "2" :
- agendar = input(' Agendar compromisso: ')
+ agendar = input('Agendar compromisso: ')
 elif opcao == "3" :
   encerramento = input(' Sair do sistema')
   print('Até mais!')
