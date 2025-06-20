10dc978b3f502c0a692b36411b58388fcad328cf
Micaela Tamires <micaela.tamir@gmail.com>
2025-06-19 22:46:13 -0300

    Update usuario.py
    Criando dicionários

--- main/usuario.py
+++ main/usuario.py
@@ -1,5 +1,20 @@
+usuarios = []
+
+nome = input('Digite o noome do usuário: ')
+data_nasc  = input(' Digite a data de nascimento: ')
+
+
 def criar_usuario():
-    print('Função de criar usuario')
+ nome = input('Digite o noome do usuário: ')
+ data_nasc  = input(' Digite a data de nascimento: ')
+ print('Função de criar usuario')
+usuario_novo = {
+    "nome": nome,
+    "data_nasc": data_nasc
+ }
+usuarios.append(usuario_novo)
+print(f'Usuario {nome} criado com sucesso!')
+
 def lista_usuario():
     print('Listar de usuarios')
 def  buscar_usuario():
