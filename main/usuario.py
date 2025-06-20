f1de1b7cc4db93316710ea6c1466154106fabe7e
Micaela Tamires <micaela.tamir@gmail.com>
2025-06-19 23:01:13 -0300

    update

--- main/usuario.py
+++ main/usuario.py
@@ -1,19 +1,16 @@
+
 usuarios = []
 
-nome = input('Digite o noome do usuário: ')
-data_nasc  = input(' Digite a data de nascimento: ')
 
-
-def criar_usuario():
- nome = input('Digite o noome do usuário: ')
+def criar_usuario() :
+ nome = input('Digite o nome do usuário: ')
  data_nasc  = input(' Digite a data de nascimento: ')
- print('Função de criar usuario')
-usuario_novo = {
-    "nome": nome,
-    "data_nasc": data_nasc
- }
-usuarios.append(usuario_novo)
-print(f'Usuario {nome} criado com sucesso!')
+ usuario_novo = {
+     "nome": nome,
+     "data_nasc": data_nasc
+}
+ usuarios.append(usuario_novo)
+ print(f'Usuario {nome} criado com sucesso!')
 
 def lista_usuario():
     print('Listar de usuarios')
