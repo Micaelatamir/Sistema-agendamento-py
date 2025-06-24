d6dbd9fa7f24b8a9d266f0c151eb1ac2066e9f7e
Micaela Tamires <micaela.tamir@gmail.com>
2025-06-24 11:28:50 -0300

    update agendamentos.py

--- main/agendamento.py
+++ main/agendamento.py
@@ -1,7 +1,22 @@
-def criar_agendamento():
-    print('Função de criar agendamento')
+agendamentos =[]
+
+def criar_agendamento() :
+ compromisso = input('nome do evento: ')
+ data_compromisso = input('data do evento:')
+ agendamento_novo = {
+         'evento': compromisso, 
+         'data':  data_compromisso
+ 
+ }
+
+ agendamentos.append(agendamento_novo)
+ print('Evento criado com sucesso')
+
 def listar_agendamento():
-    print('listar agendamento') 
+  nomel = input('Digite o nome :') 
+  data_evento = input()
 def cancelar_agendamento():
     print('Cancelar de agendamento')
     
+
+
