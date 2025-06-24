21b5411f420bb5e2e9e5a41dd31d5e9161337406
Micaela Tamires <micaela.tamir@gmail.com>
2025-06-24 11:46:01 -0300

    Update agendamento.py

--- main/agendamento.py
+++ main/agendamento.py
@@ -15,10 +15,15 @@ def listar_agendamento():
  print('Lista de agendamentos')
  for agendamento in agendamentos :
    print(f'{agendamento['nome']} e data do evento{agendamento['Data']} ')
-   
+
 
 def cancelar_agendamento():
-    print('Cancelar de agendamento')
-    
+ cancelar_agendamento = input( 'Digite o nome do usuario para deletar :')
+ for agendamento in agendamentos:
+   if agendamento == cancelar_agendamento:
+    agendamentos.remove(agendamento)   
+   print(f' usuario{cancelar_agendamento} com sucesso!')
+   return
+print('usuario inexistente')
 
 
